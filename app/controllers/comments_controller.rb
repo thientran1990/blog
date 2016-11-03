e5f165c83759c0e564f0eaf2a9b6aaf5f8f589b2
thientran1990 <thientran1996.com>
2016-11-03 16:51:36 +0700

    Destroy comment with Ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,13 +10,18 @@ class CommentsController < ApplicationController
       format.js   {}
     end
   end
- 
+
   def destroy
+
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
-    redirect_to article_path(@article)
-
+    #redirect_to article_path(@article)
+    respond_to do |format|
+      format.html {redirect_to article_path(@article),notice: 'test'}
+      format.json {head :no_content}
+      format.js {render layout:false}
+    end
   end
   private
     def comment_params
